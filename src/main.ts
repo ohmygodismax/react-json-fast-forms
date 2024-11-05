583fdb0cf3cedf06c0742f7d4966567aaec91891
mkulakov <mkulakov_niitp@inbox.ru>
2024-11-05 12:23:01 +0300

    build: Added missing exports.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 export {FormConfiguration} from "@/components/FormConfiguration.tsx";
 export {DynamicForm} from "@/components/DynamicForm.tsx";
 
-export type * from "@/models/FormMeta.ts"
+export type {FormScheme} from "@/models/scheme/FormScheme";
+export type {SchemeComponent} from "@/models/scheme/SchemeComponent";
+export type {SchemeComponentTypes} from "@/models/scheme/SchemeComponentTypes";
+export type {FormState} from "@/models/FormState";
+export type {FormMeta} from "@/models/FormMeta";
+
+export {SubmitButton} from "@/containers/SubmitButton";
