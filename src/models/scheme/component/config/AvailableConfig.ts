4ee00049f99da83081661c6ae5c04c898e400226
mkulakov <mkulakov_niitp@inbox.ru>
2024-11-14 14:17:51 +0300

    fix: The name of the accessibility method has been changed.

--- src/models/scheme/component/config/AvailableConfig.ts
+++ src/models/scheme/component/config/AvailableConfig.ts
@@ -2,5 +2,5 @@ import {JSONFunction} from "@/models/scheme/component/parts/JSONFunction.ts";
 
 export type AvailableConfig = {
 	readonly?: boolean,
-	dependsConditions?: JSONFunction //input: form state
+	dependsCondition?: JSONFunction //input: form state
 }
