b48810c07a8f2c4159ff66a0932eea9d9af667d6
mkulakov <mkulakov_niitp@inbox.ru>
2024-11-14 14:17:24 +0300

    feat: A select adapter for async data has been created.

---
+++ src/components/async/AsyncSelectAdapter.tsx
@@ -0,0 +1,30 @@
+import {AsyncData} from "@/components/async/AsyncDataProvider.tsx";
+import {ReactElement, useMemo} from "react";
+import {FabricException} from "@/helpers/exceptions/FabricException.ts";
+
+interface AsyncSelectAdapterProps {
+	data: AsyncData,
+	render: (options: {value: string, label: string}[]) => ReactElement
+}
+
+export const AsyncSelectAdapter = ({data, render}: AsyncSelectAdapterProps) => {
+	const options = useMemo(() => {
+		if (data) {
+			return data.map((item) => {
+				const {value, label} = item;
+				if (!value || !label) {
+					throw FabricException('value || label');
+				}
+				return {value, label}
+			})
+		} else {
+			return []
+		}
+	}, [data])
+
+	return (
+		<>
+			{render(options)}
+		</>
+	)
+}
