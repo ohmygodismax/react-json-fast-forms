957f31719cd6336691fcd3eb10a7c69929eaab1e
mkulakov <mkulakov_niitp@inbox.ru>
2024-11-15 08:45:50 +0300

    feat: Added a method for tracking form changes.

--- src/components/DynamicForm.tsx
+++ src/components/DynamicForm.tsx
@@ -8,12 +8,13 @@ import {FormConfig} from "@/models/FormConfig.ts";
 
 interface DynamicFormProps {
 	config: FormConfig,
-	onFinish: (state: FormState) => void,
+	onFinish?: (state: FormState) => void,
+	onChange?: (state: FormState) => void,
 	submitButton?: ReactElement,
 	isDisabled?: boolean,
 }
 
-export const DynamicForm = ({config, onFinish, isDisabled = false, submitButton}: DynamicFormProps) => {
+export const DynamicForm = ({config, onChange, onFinish, isDisabled = false, submitButton}: DynamicFormProps) => {
 	const {	token: { colorBgBase }	} = theme.useToken();
 	const [form] = useForm<FormState>();
 
@@ -24,6 +25,7 @@ export const DynamicForm = ({config, onFinish, isDisabled = false, submitButton}
 			labelAlign={'left'}
 			initialValues={config.state}
 			onFinish={onFinish}
+			onValuesChange={onChange}
 			style={{width:'100%', background: `${colorBgBase}`}}
 			disabled={isDisabled}
 		>
