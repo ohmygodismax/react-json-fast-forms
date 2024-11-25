f33d3b8ec686eb8cd058b427e10e7bce6fe7c935
mkulakov <mkulakov_niitp@inbox.ru>
2024-11-25 15:54:29 +0300

    perf/fix: A little order has been put in the configurations. A typo has been fixed.

--- src/models/scheme/component/config/AsyncConfig.ts
+++ src/models/scheme/component/config/AsyncConfig.ts
@@ -1,9 +1,11 @@
 import {JSONFunction} from "@/models/scheme/component/parts/JSONFunction.ts";
 
+export type AsyncType = 'fetch/post' | 'fetch/get' | 'gql';
+
 export type AsyncConfig = {
-	type: 'fetch/post' | 'fetch/get' | 'gql',
+	type: AsyncType,
 	url: string,
-	query: string,
+	query?: string,
 	data?: {
 		intersectProcessing?: JSONFunction //input: data, state //TODO: Подумать над названием и местом в конфигурации.
 	},
@@ -11,14 +13,9 @@ export type AsyncConfig = {
 		dependsCondition?: JSONFunction //input: state TODO: Переименовать когда определится полная структура
 		stateToFetchVariables?: JSONFunction//input: state. Convert state to fetch variables
 		watchedVariables?: string[] //ReFetch only if variables value change
-	}
-	depends?: Array<{
-		value: string,
-		path?: string | string[],
-		wrappedAsyncValue?: string
-	}>,
+	},
 	extractors?: { //input: processing data
-		value: JSONFunction,
-		label: JSONFunction
+		value?: JSONFunction,
+		label?: JSONFunction
 	}
 }
--- src/models/scheme/component/config/LayoutConfig.ts
+++ src/models/scheme/component/config/LayoutConfig.ts
@@ -2,9 +2,11 @@ import {SpanOffset} from "@/models/parts/layout/SpanOffset.ts";
 import {BreakPointsLayout} from "@/models/parts/layout/BreakPointsLayout.ts";
 import {Direction} from "@/models/parts/layout/Direction.ts";
 
+export type Align = "left" | "center" | "right";
+
 export type LayoutConfig = {
 	labelCol?: SpanOffset | BreakPointsLayout,
 	wrapperCol?: SpanOffset | BreakPointsLayout,
 	direction?: Direction,
-	align?: 'left' | 'center' | 'right'
+	align?: Align
 }
